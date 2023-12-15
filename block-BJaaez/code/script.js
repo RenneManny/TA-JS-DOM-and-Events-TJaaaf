0bf6e5d3c0a4292b73eb7dcced4149dd6cda3985
Akhil Kumar <akhilkum249@gmail.com>
2023-12-15 14:14:50 +0530

    To-Do list extension

--- block-BJaaez/code/script.js
+++ block-BJaaez/code/script.js
@@ -1,12 +1,61 @@
-document.addEventListener('DOMContentLoaded', function () {
-    var btn = document.querySelector('#openChatGPT');
-  
-    // Function to open ChatGPT
-    function openGPT() {
-      chrome.tabs.create({ url: 'https://www.chatgpt.com/' });
+function main() {
+  let inputTask = document.querySelector("#task-input");
+  let root = document.querySelector("ul");
+  let allTasks = JSON.parse(localStorage.getItem("tasks")) || [];
+
+  function handleTask(event) {
+    let value = event.target.value;
+    if (event.keyCode == "13" && value !== "") {
+      let task = {
+        name: value,
+        isDone: false,
+      };
+      allTasks.push(task);
+      event.target.value = "";
+      createUi();
     }
-  
-    // Add click event listener to the button
-    btn.addEventListener('click', openGPT);
-  });
-  
+    localStorage.setItem("tasks", JSON.stringify(allTasks));
+  }
+  // handle toggle
+  function handleToggle(event) {
+    let id = event.target.dataset.id;
+    allTasks[id].isDone = !allTasks[id].isDone;
+    localStorage.setItem("tasks", JSON.stringify(allTasks));
+    createUi();
+  }
+  // delete function
+
+  function delFun(event) {
+    let id = event.target.dataset.id;
+    allTasks.splice(id, 1);
+    localStorage.setItem("tasks", JSON.stringify(allTasks));
+    createUi();
+  }
+  // creating ui
+  function createUi() {
+    root.innerHTML = "";
+    allTasks.forEach((task, index) => {
+      let li = document.createElement("li");
+      let input = document.createElement("input");
+      input.type = "checkbox";
+      input.addEventListener("change", handleToggle);
+      input.checked = task.isDone;
+      input.setAttribute("data-id", index);
+
+      let p = document.createElement("p");
+      p.innerText = task.name;
+      if (task.isDone) {
+        p.classList.add("completed");
+      }
+
+      let icon = document.createElement("i");
+      icon.classList.add("fas", "fa-trash", "trashcol");
+      icon.addEventListener("click", delFun);
+      icon.setAttribute("data-id", index);
+      li.append(p, input, icon);
+      root.append(li);
+    });
+  }
+  inputTask.addEventListener("keyup", handleTask);
+}
+main();
